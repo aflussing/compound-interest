d90fb771be62a3ac349cc980ad0880bd283cad17
Alphonsus F. Lussing <35009042+aflussing@users.noreply.github.com>
2020-04-04 18:51:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 interestRate = 1.05
 amount = 500 # by months
 years = 40
-result = 0
-annualAmount = amount * 12
 
-for i in range(years):
-  result = (result + annualAmount) * interestRate
-  print('Amount per year', i, ' :', result)
+
+
+def calculate(amount, interestRate, years):
+  annualAmount = amount * 12
+  result = 0
+  for i in range(years):
+    result = (result + annualAmount) * interestRate
+    print('Amount per year', i, ' :', result)
+
+  
